f603848190a1042cd45b1e29fa6a658d1caf7fc6
Zetaphor <zetaphor@gmail.com>
2020-05-26 16:04:37 -0400

    Cleanup

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 import * as THREE from './three.module.js';
-import { GLTFExporter } from './GLTFExporter.js';
+import {
+  GLTFExporter
+} from './GLTFExporter.js';
 let camera, scene, renderer, container;
 let conLeft, conRight, xrConLeft, xrConRight;
 let light, cubeInterval, cubeColor;
@@ -26,20 +28,26 @@ function init() {
   light = new THREE.HemisphereLight(0xffffbb, 0x080820, 1);
   scene.add(light);
 
-  conLeft = new THREE.Mesh(new THREE.BoxGeometry(0.1, 0.1, 0.1), new THREE.MeshLambertMaterial({ color: 0xff0000 }));
-  conRight = new THREE.Mesh(new THREE.BoxGeometry(0.1, 0.1, 0.1), new THREE.MeshLambertMaterial({ color: 0x0000ff }));
+  conLeft = new THREE.Mesh(new THREE.BoxGeometry(0.1, 0.1, 0.1), new THREE.MeshLambertMaterial({
+    color: 0xff0000
+  }));
+  conRight = new THREE.Mesh(new THREE.BoxGeometry(0.1, 0.1, 0.1), new THREE.MeshLambertMaterial({
+    color: 0x0000ff
+  }));
   conLeft.visible = false;
   conRight.visible = false;
   scene.add(conLeft, conRight);
 
   scene.add(markers);
 
-  renderer = new THREE.WebGLRenderer({ alpha: true });
+  renderer = new THREE.WebGLRenderer({
+    alpha: true
+  });
   renderer.setSize(window.innerWidth, window.innerHeight);
   document.body.appendChild(renderer.domElement);
   renderer.xr.enabled = true;
 
-  document.addEventListener('click', function(e) {
+  document.addEventListener('click', function (e) {
     if (e.target.id === 'outputPoints') outputPoints();
     if (e.target.id === 'downloadPoints') downloadPoints();
     else if (e.target.id === 'importPoints') importPoints();
@@ -51,7 +59,7 @@ function requestSession() {
   totalPointsEl.parentElement.style.display = 'none';
   statusEl.parentElement.style.display = 'block';
   statusEl.innerHTML = 'Requesting WebXR session';
-  navigator.xr.isSessionSupported('immersive-vr').then(function (supported) {
+  navigator.xr.isSessionSupported('immersive-vr').then(function () {
     let options = { optionalFeatures: ['local-floor', 'bounded-floor'] };
     navigator.xr.requestSession('immersive-vr', options).then(onSessionStarted).catch(function (err) {
       totalPointsEl.parentElement.style.display = 'none';
@@ -151,7 +159,9 @@ function downloadScene() {
   var exporter = new GLTFExporter();
   exporter.parse(scene, function (gltf) {
     download('gltf', JSON.stringify(gltf));
-  }, { truncateDrawRange: false });
+  }, {
+    truncateDrawRange: false
+  });
 }
 
 function downloadPoints() {
@@ -172,7 +182,7 @@ function download(extension, data) {
 function dateTimeString() {
   let today = new Date();
   let ampm = (today.getHours() >= 12) ? "PM" : "AM";
-  let date = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
+  let date = today.getFullYear() + '-' + (today.getMonth() + 1) + '-' + today.getDate();
   let time = today.getHours() + "." + today.getMinutes() + "." + today.getSeconds();
   return `${date}_${time}${ampm}`;
 }
