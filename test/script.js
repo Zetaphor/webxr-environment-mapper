536c52c43c16777922d42f91acca802228244b3c
Zetaphor <zetaphor@gmail.com>
2020-05-27 22:58:56 -0400

    Initial commit

---
+++ test/script.js
@@ -0,0 +1,81 @@
+import * as THREE from './three.module.js';
+import {
+  GLTFLoader
+} from './GLTFLoader.js';
+
+let camera, scene, renderer, container;
+let conLeft, conRight, xrConLeft, xrConRight;
+let light;
+
+init();
+requestSession();
+
+window.addEventListener("unload", closeSession);
+
+function init() {
+  container = document.createElement('div');
+  document.body.appendChild(container);
+
+  scene = new THREE.Scene();
+  camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+  camera.position.z = 5;
+
+  light = new THREE.HemisphereLight(0xffffbb, 0x080820, 1);
+  scene.add(light);
+
+  conLeft = new THREE.Mesh(new THREE.BoxGeometry(0.1, 0.1, 0.1), new THREE.MeshLambertMaterial({ color: 0xff0000 }));
+  conRight = new THREE.Mesh(new THREE.BoxGeometry(0.1, 0.1, 0.1), new THREE.MeshLambertMaterial({ color: 0x0000ff }));
+  scene.add(conLeft, conRight);
+
+  const gltfLoader = new GLTFLoader();
+  const url = 'localhost-8000-webxr-environment-mapper-personal-aframe-html.glb';
+  gltfLoader.load(url, (gltf) => {
+    const root = gltf.scene;
+    scene.add(root);
+  });
+
+  renderer = new THREE.WebGLRenderer({ alpha: true });
+  renderer.setSize(window.innerWidth, window.innerHeight);
+  document.body.appendChild(renderer.domElement);
+  renderer.xr.enabled = true;
+}
+
+function requestSession() {
+  navigator.xr.isSessionSupported('immersive-vr').then(function (supported) {
+    let options = { optionalFeatures: ['local-floor', 'bounded-floor'] };
+    navigator.xr.requestSession('immersive-vr', options).then(onSessionStarted);
+  });
+}
+
+function onSessionStarted(session) {
+  renderer.xr.setSession(session);
+  xrConLeft = renderer.xr.getController(0);
+  xrConRight = renderer.xr.getController(1);
+  animate();
+}
+
+async function closeSession(session) {
+  await renderer.xr.getSession().end();
+}
+
+function animate() {
+  renderer.setAnimationLoop(render);
+}
+
+function render() {
+  conLeft.position.x = xrConLeft.position.x;
+  conLeft.position.y = xrConLeft.position.y;
+  conLeft.position.z = xrConLeft.position.z;
+  conLeft.rotation.x = xrConLeft.rotation.x;
+  conLeft.rotation.y = xrConLeft.rotation.y;
+  conLeft.rotation.z = xrConLeft.rotation.z;
+
+  conRight.position.x = xrConRight.position.x;
+  conRight.position.y = xrConRight.position.y;
+  conRight.position.z = xrConRight.position.z;
+  conRight.rotation.x = xrConRight.rotation.x;
+  conRight.rotation.y = xrConRight.rotation.y;
+  conRight.rotation.z = xrConRight.rotation.z;
+
+  renderer.render(scene, camera);
+}
